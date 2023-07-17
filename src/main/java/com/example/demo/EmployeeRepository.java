78f1cc5ee92243cad73924e0ffdd65bc12409034
Ernesto Anaya <ernestex@gmail.com>
2023-07-17 17:27:54 +0000

    bonus

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(collectionResourceRel = "empleados", path = "empleados")
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
